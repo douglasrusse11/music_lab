d5a53416d58e180ab2b37b2eac6f31e47830fd96
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-09 19:56:41 +0000

    adds save in album_repository

--- console.py
+++ console.py
@@ -11,3 +11,24 @@ artist_repository.save(brainbombs)
 
 boy_harsher = Artist("Boy Harsher")
 artist_repository.save(boy_harsher)
+
+urge_to_kill = Album("Urge to Kill", "Noise Rock", brainbombs)
+album_repository.save(urge_to_kill)
+
+obey = Album("Obey", "Noise Rock", brainbombs)
+album_repository.save(obey)
+
+burning_hell = Album("Burning Hell", "Noise Rock", brainbombs)
+album_repository.save(burning_hell)
+
+genius_and_brutality = Album("Genius and Brutality - Taste and Power", "Noise Rock", brainbombs)
+album_repository.save(genius_and_brutality)
+
+careful = Album("Careful", "Minimal Synth", boy_harsher)
+album_repository.save(careful)
+
+country_girl_uncut = Album("Country Girl Uncut", "Minimal Synth", boy_harsher)
+album_repository.save(country_girl_uncut)
+
+yr_body_is_nothing = Album("Yr Body Is Nothing", "Minimal Synth", boy_harsher)
+album_repository.save(yr_body_is_nothing)
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -2,3 +2,10 @@ from db.run_sql import run_sql
 from models.album import Album
 from models.artist import Artist
 import repositories.artist_repository as artist_repository
+
+def save(album):
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%(title)s, %(genre)s, %(artist_id)s) RETURNING *"
+    values = {'title': album.title, 'genre': album.genre, 'artist_id': album.artist.id}
+    result = run_sql(sql, values)
+    id = result[0]["id"]
+    album.id = id
