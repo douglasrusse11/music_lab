5e88daf1ef89ebcd9bf6f93959657e9bcf095cc1
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-09 19:54:23 +0000

    adds save in artist_repository

--- console.py
+++ console.py
@@ -5,3 +5,9 @@ import repositories.album_repository as album_repository
 import os 
 
 os.system('psql -d music_collection -f db/music_collection.sql')
+
+brainbombs = Artist("Brainbombs")
+artist_repository.save(brainbombs)
+
+boy_harsher = Artist("Boy Harsher")
+artist_repository.save(boy_harsher)
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -1,3 +1,10 @@
 from db.run_sql import run_sql
 from models.artist import Artist
 from models.album import Album
+
+def save(artist):
+    sql = "INSERT INTO artists (name) VALUES (%s) RETURNING *"
+    values = [artist.name]
+    result = run_sql(sql, values)
+    id = result[0]["id"]
+    artist.id = id 
