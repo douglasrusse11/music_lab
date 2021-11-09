bd5c880c27e37a681ad868fa3fe69d244368757e
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-09 19:40:31 +0000

    adds init tests for Artist

--- tests/artist_test.py
+++ tests/artist_test.py
@@ -2,4 +2,16 @@ import unittest
 from models.artist import Artist
 
 class TestArtist(unittest.TestCase):
-    pass
+    def test_artist_has_name(self):
+        self.artist = Artist("Brainbombs")
+        self.assertEqual("Brainbombs", self.artist.name)
+
+    @unittest.skip('')
+    def test_artist_id_is_None(self):
+        self.artist = Artist("Brainbombs")
+        self.assertIsNone(self.artist.id)
+
+    @unittest.skip('')
+    def test_artist_has_id(self):
+        self.artist = Artist("Brainbombs", 1)
+        self.assertEqual(1, self.artist.id)
